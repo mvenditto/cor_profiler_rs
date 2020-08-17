5a6a5086f9df17d32974528a384c5a64a622b969
mvenditto <venditto.matteo@gmail.com>
2020-08-17 20:09:59 +0200

    add IMetaDataDispender com interface

---
+++ cor_profiler/src/interfaces/i_meta_data_dispenser.rs
@@ -0,0 +1,15 @@
+use crate::types::*;
+
+use std::ffi::c_void;
+
+extern crate com;
+use com::{
+    com_interface,
+    interfaces::iunknown::IUnknown,
+    sys::HRESULT,
+};
+
+#[com_interface("809C652E-7396-11D2-9771-00A0C9B4D50C")]
+pub trait IMetaDataDispenser: IUnknown {
+    unsafe fn define_scope(&self, rclsid: REFCLSID, dw_create_flags: DWORD, riid: REFIID, ppiunkn: *mut *mut dyn IUnknown) -> HRESULT;
+}
