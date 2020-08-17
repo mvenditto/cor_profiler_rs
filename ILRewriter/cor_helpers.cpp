ca28acd679441a94eaf0e187ffb8235c2e52ac45
mvenditto <venditto.matteo@gmail.com>
2020-08-17 11:57:39 +0200

    add few clr helper to be used in future tests

--- ILRewriter/cor_helpers.cpp
+++ ILRewriter/cor_helpers.cpp
@@ -1,5 +1,38 @@
 #include "cor_helpers.h"
 #include <iostream>
+#include <vector>
+
+HRESULT __cdecl clr_create_meta_host(C_ICLRMetaHost* out_metahost)
+{
+    return CLRCreateInstance(CLSID_CLRMetaHost, IID_ICLRMetaHost, (void**)&out_metahost);
+}
+
+HRESULT __cdecl clr_get_installed_runtimes(
+    C_ICLRMetaHost metahost, 
+    C_ICLRRuntimeInfo* installed_runtimes_out,
+    ULONG* installed_runtimes_length
+) {
+    auto _metahost = reinterpret_cast<ICLRMetaHost*>(metahost);
+    IEnumUnknown* runtimes = nullptr;
+    auto hr = _metahost->EnumerateInstalledRuntimes(&runtimes);
+    ICLRRuntimeInfo* runtime = nullptr;
+    ULONG fetched = 0;
+    std::vector<ICLRRuntimeInfo> *vect = new std::vector<ICLRRuntimeInfo>;
+    while ((hr = runtimes->Next(1, (IUnknown**)&runtime, &fetched)) == S_OK && fetched > 0) {
+        vect->push_back(*runtime);
+    }
+    installed_runtimes_out = (C_ICLRRuntimeInfo*)(&vect->at(0));
+    *installed_runtimes_length = (ULONG) vect->size();
+}
+
+HRESULT __cdecl clr_runtime_get_metadata_dispenser(C_ICLRRuntimeInfo* runtime, IMetaDataDispenser* metadata_dispenser_out) {
+    auto _runtime = reinterpret_cast<ICLRRuntimeInfo*>(runtime);
+    auto hr = _runtime->GetInterface(
+            CLSID_CorMetaDataDispenser, 
+            IID_IMetaDataDispenser,
+            (void**)&metadata_dispenser_out);
+    return hr;
+}
 
 UINT __cdecl cor_sig_compress_token(mdToken token, void* out_buffer)
 {
--- ILRewriter/cor_helpers.h
+++ ILRewriter/cor_helpers.h
@@ -1,16 +1,28 @@
 #pragma once
 #include <corhlpr.h>
+#include <metahost.h>
 
 
 extern "C" {
 
-	__declspec(dllexport) UINT __cdecl cor_sig_compress_token(
-		mdToken token,
-		void* out_buffer
-	);
+    typedef void* C_ICLRMetaHost;
+    typedef void* C_ICLRRuntimeInfo;
 
-	__declspec(dllexport) COR_SIGNATURE* cor_sig_compress_token_2(mdToken token, ULONG* compressed_tk_length);
+    __declspec(dllexport) HRESULT clr_create_meta_host(C_ICLRMetaHost* out_metahost);
 
-	__declspec(dllexport) mdToken cor_sig_uncompress_token_2(COR_SIGNATURE* sig, ULONG* uncompressed_tk_length);
+    __declspec(dllexport) HRESULT  clr_get_installed_runtimes(
+        C_ICLRMetaHost metahost, C_ICLRRuntimeInfo* installed_runtimes_out, ULONG* installed_runtimes_length);
+
+    __declspec(dllexport) HRESULT clr_runtime_get_metadata_dispenser(
+        C_ICLRRuntimeInfo* runtime, IMetaDataDispenser* metadata_dispenser_out);
+
+	__declspec(dllexport) UINT cor_sig_compress_token(
+        mdToken token, void* out_buffer);
+
+	__declspec(dllexport) COR_SIGNATURE* cor_sig_compress_token_2(
+        mdToken token, ULONG* compressed_tk_length);
+
+	__declspec(dllexport) mdToken cor_sig_uncompress_token_2(
+        COR_SIGNATURE* sig, ULONG* uncompressed_tk_length);
 
 }
