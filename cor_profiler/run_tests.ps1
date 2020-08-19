eee2242d0c3f3433b70dee796d4d3cdc822c5269
mvenditto <venditto.matteo@gmail.com>
2020-08-19 15:24:01 +0200

    redirect stderr in run_tests to null to suppress warnings prints

--- cor_profiler/run_tests.ps1
+++ cor_profiler/run_tests.ps1
@@ -7,8 +7,8 @@ $env:RUST_LOG="info"
 
 if($run_all_tests -or -not $test_name) {
     $cmd_args = @('test', '--', '--nocapture', '--test-threads=1')
-    & 'cargo' $cmd_args
+    & 'cargo' $cmd_args 2> $null
 } else {
     $cmd_args = @('test', '--', '--nocapture', $test_name)
-    & 'cargo' $cmd_args
+    & 'cargo' $cmd_args 2> $null
 }
